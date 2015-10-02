3615fa046b2866c99e1d580f1a3feeccaba3af9e
Serge Martial NGUETTA <sm.nguetta@gmail.com>
2015-10-03 00:10:17 +0200

    Created Main class

---
+++ src/main/java/fr/polytech/mercury/Main.java
@@ -0,0 +1,12 @@
+package fr.polytech.mercury;
+
+/**
+ * Main Class to run the Program
+ *
+ * @author Serge Martial N.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
